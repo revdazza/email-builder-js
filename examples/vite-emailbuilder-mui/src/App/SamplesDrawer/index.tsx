e3edb00688425c22f2227b2d1cc492639f69c638
Darren Atwater <atwater@gmail.com>
2026-03-17 18:39:46 -0400

    Remove Waypoint ad from samples drawer

--- examples/vite-emailbuilder-mui/src/App/SamplesDrawer/index.tsx
+++ examples/vite-emailbuilder-mui/src/App/SamplesDrawer/index.tsx
@@ -50,29 +50,6 @@ export default function SamplesDrawer() {
             </Button>
           </Stack>
         </Stack>
-        <Stack spacing={2} px={0.75} py={3}>
-          <Link href="https://usewaypoint.com?utm_source=emailbuilderjs" target="_blank" sx={{ lineHeight: 1 }}>
-            <Box component="img" src={logo} width={32} />
-          </Link>
-          <Box>
-            <Typography variant="overline" gutterBottom>
-              Looking to send emails?
-            </Typography>
-            <Typography variant="body2" color="text.secondary" paragraph>
-              Waypoint is an end-to-end email API with a &apos;pro&apos; version of this template builder with dynamic
-              variables, loops, conditionals, drag and drop, layouts, and more.
-            </Typography>
-          </Box>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ justifyContent: 'center' }}
-            href="https://usewaypoint.com?utm_source=emailbuilderjs"
-            target="_blank"
-          >
-            Learn more
-          </Button>
-        </Stack>
       </Stack>
     </Drawer>
   );
